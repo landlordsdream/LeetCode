55447b3dedb8adba496615c6571572456bc0ec9c
liule <liule7481@gmail.com>
2026-09-12 16:22:39 +0800

    update 160 use length difference approach

--- 160_intersection_of_two_linked_lists.py
+++ 160_intersection_of_two_linked_lists.py
@@ -6,6 +6,7 @@ class ListNode:
 
 from typing import Optional
 
+# 交换法 pa pb 均遍历两链表
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if headA is None or headB is None:
@@ -17,4 +18,35 @@ class Solution:
             pb = pb.next if pb else headA
         return pa
     
-
+# 链表长度差值法
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA is None or headB is None:
+            return None
+        pa = headA
+        pb = headB
+        lenA = 0
+        lenB = 0
+        while pa:
+            lenA += 1
+            pa = pa.next
+        while pb:
+            lenB += 1
+            pb = pb.next
+        pa = headA
+        pb = headB
+        if lenA > lenB:
+            for _ in range(lenA - lenB):
+                pa = pa.next
+        else:
+            for _ in range(lenB - lenA):
+                pb = pb.next
+        while pa and pb:
+            if pa == pb:
+                return pa
+            pa = pa.next
+            pb = pb.next
+        return pa
+        
+        
+        
